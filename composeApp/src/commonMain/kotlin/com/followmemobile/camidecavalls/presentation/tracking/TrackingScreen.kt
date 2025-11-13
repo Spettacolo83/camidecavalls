9f0f3d9798656a154a394315db6343f9c5dcd578
Stefano Russello <developer@stefanorussello.it>
2025-11-13 22:43:22 +0100

    Add missing tracking resources and import back string

--- composeApp/src/commonMain/kotlin/com/followmemobile/camidecavalls/presentation/tracking/TrackingScreen.kt
+++ composeApp/src/commonMain/kotlin/com/followmemobile/camidecavalls/presentation/tracking/TrackingScreen.kt
@@ -27,6 +27,7 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import camidecavalls.composeapp.generated.resources.Res
+import camidecavalls.composeapp.generated.resources.back
 import camidecavalls.composeapp.generated.resources.tracking_acquiring_signal
 import camidecavalls.composeapp.generated.resources.tracking_avg_speed
 import camidecavalls.composeapp.generated.resources.tracking_completed
